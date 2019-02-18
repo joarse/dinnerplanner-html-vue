17c83a5ac2d73208ece3df75b163792bf17dd31a
joarse <joarse@kth.se>
2019-02-18 10:58:35 +0100

    first commit

---
+++ src/components/DishDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="details">
+    <h2>This is the dish details screen</h2>
+    <!-- We pass the model as property to the Sidebar component -->
+    <sidebar :model="this.model"/>
+    <dishes />
+  </div>
+</template>
+
+<script>
+  import Sidebar from '@/components/Sidebar'
+  import Dishes from '@/components/Dishes'
+
+  export default {
+    props: ['model'],
+    components: {
+      'sidebar': Sidebar,
+    }
+  }
+</script>
