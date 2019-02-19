f6d5d04f357484e8426263e86b1ff99ada8df8ff
Jy-Liu <junyouliu9@gmail.com>
2019-02-19 10:51:37 +0100

    detail view

---
+++ src/components/Detail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Detail">
+    <h3>Details</h3>
+    <p>
+      {{ text }}
+    </p>
+  </div>
+</template>
+
+<script>
+  // Alternative to passing the moderl as the component property,
+  // we can import the model instance directly
+  import modelInstance from "../data/DinnerModel";
+
+  export default {
+    data() {
+      return {
+        text: modelInstance.getNumberOfGuests()
+      }
+    },
+
+    methods: {
+    }
+  }
+</script>
