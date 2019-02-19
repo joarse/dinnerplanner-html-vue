4b2df87f54fcc1b882787ec4c160f01e1bedc32d
Jy-Liu <junyouliu9@gmail.com>
2019-02-19 10:51:18 +0100

    add detail component

--- src/components/DishDetail.vue
+++ src/components/DishDetail.vue
@@ -3,18 +3,19 @@
     <h2>This is the dish details screen</h2>
     <!-- We pass the model as property to the Sidebar component -->
     <sidebar :model="this.model"/>
-    <dishes />
+    <detail />
   </div>
 </template>
 
 <script>
   import Sidebar from '@/components/Sidebar'
-  import Dishes from '@/components/Dishes'
+  import Detail from '@/components/Detail'
 
   export default {
     props: ['model'],
     components: {
       'sidebar': Sidebar,
+      'detail': Detail
     }
   }
 </script>
