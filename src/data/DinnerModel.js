3787c8da970fc0ff8fa45a0376db70d2bb7e698b
Jy-Liu <junyouliu9@gmail.com>
2019-02-19 09:42:30 +0100

    get detailed info of a dish + fix some structures

--- src/data/DinnerModel.js
+++ src/data/DinnerModel.js
@@ -1,15 +1,18 @@
 import ObservableModel from "./ObservableModel";
 
-const BASE_URL = "http://sunset.nada.kth.se:8080/iprog/group/49";
-const httpOptions = {
-  headers: { "X-Mashape-Key": "" }
-};
+
 
 class DinnerModel extends ObservableModel {
   constructor() {
     super();
     this._numberOfGuests = 4;
     this.getNumberOfGuests();
+
+    this.apiKey = "";
+    this.BASE_URL = "http://sunset.nada.kth.se:8080/iprog/group/49";
+    this.httpOptions = {
+      headers: { "X-Mashape-Key": this.apiKey }
+    };
   }
 
   /**
@@ -35,20 +38,36 @@ class DinnerModel extends ObservableModel {
 
   // API methods
 
-  /**
-   * Do an API call to the search API endpoint.
-   * @returns {Promise<any>}
-   */
-  getAllDishes() {
-    const url = `${BASE_URL}/recipes/search`;
-    return fetch(url, httpOptions).then(this.processResponse);
-  }
-
   processResponse(response) {
     if (response.ok) {
       return response.json();
     }
     throw response;
+  }
+
+  /**
+   * Do an API call to the search API endpoint.
+   * @returns {Promise<any>}
+   */
+  getAllDishes() {
+    const url = `${this.BASE_URL}/recipes/search`;
+    return fetch(url, this.httpOptions)
+      .then(response => this.processResponse(response));
+  }
+
+  /**
+   * Do an API call to get the detailed info of dish `id`.
+   * @returns {Promise<any>}
+   */
+  getDetailedInfo(id) {
+    const url = `${this.BASE_URL}/recipes/${id}/information`;
+
+    return fetch(`${url}?includeNutrition=false`, this.httpOptions)
+      .the(response => this.processResponse(response))
+      .catch(e => {
+        alert("There's something wrong while getting detailed info of a dish");
+        console.log(e);
+      });
   }
 }
 
