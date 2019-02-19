d2a06745a3fdeb47f6f6a1043b774ca69c56474c
joarse <joarse@kth.se>
2019-02-19 11:23:43 +0100

    added text queries for searching

--- src/components/SelectDish.vue
+++ src/components/SelectDish.vue
@@ -2,7 +2,16 @@
   <div class="select-dish">
     <h2>This is the Select Dish screen</h2>
     <!-- We pass the model as property to the Sidebar component -->
-    <sidebar :model="this.model"/>
+    <input type="text"
+           :value="text"
+           placeholder="edit me"
+           @input="search">
+    <button @click="search">
+      SEARCH
+    </button>
+    <p>Message is: {{ text }}</p>
+
+    <sidebar :model="this.model" />
     <dishes />
   </div>
 </template>
@@ -13,9 +22,32 @@
 
   export default {
     props: ['model'],
+
+    created() {
+      this.model.addObserver(this);
+    },
+
     components: {
       'sidebar': Sidebar,
       'dishes': Dishes
+    },
+    data() {
+      if (localStorage.query) {
+        this.model.setQuery(localStorage.query);
+      }
+      return {
+        text: this.model.getQuery()
+      };
+    },
+
+    methods: {
+      search(e) {
+        this.model.setQuery(e.target.value);
+        localStorage.query = e.target.value;
+      }, 
+      update() {
+        this.text = this.model.getQuery();
+      }
     }
   }
 </script>
