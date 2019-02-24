5696c2c4fddc940b5e2cf816c8f263207ac4ce2e
Jy-Liu <junyouliu9@gmail.com>
2019-02-24 22:31:10 +0100

    fix indentation

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -4,68 +4,65 @@
     <router-link to="/search">
       <button>Go back to the menu</button>
     </router-link>
-    <p>{{dish.title}}</p>
-    <p>{{dish.id}} </p>
+    <h4>{{dish.title}}</h4>
     <li v-for="ingredient in dish.extendedIngredients" :key="ingredient.id">
       {{ingredient.name}} {{ingredient.amount}} {{ingredient.unit}} {{1}} SEK
     </li>
-    <router-link  to="/search">
+    <router-link to="/search">
       <button @click="add"> ADD TO MENU</button>
     </router-link>
   </div>
 </template>
 
 <script>
-  /***
-   * If we bind the model in the router with `props`, we don't need to import the actual file from relateive path
-   */
-  // Alternative to passing the moderl as the component property,
-  // we can import the model instance directly
-  // import modelInstance from "../data/DinnerModel";
+/***
+ * If we bind the model in the router with `props`, we don't need to import the actual file from relateive path
+ */
+// Alternative to passing the moderl as the component property,
+// we can import the model instance directly
+// import modelInstance from "../data/DinnerModel";
 
-  export default {
-    props: ["model"],
-    // this methods is called by React lifecycle when the
-    // component is created that's a good place to setup model observer
-    created() {
-      this.model.addObserver(this);
+export default {
+  props: ["model"],
+  // this methods is called by React lifecycle when the
+  // component is created that's a good place to setup model observer
+  created() {
+    this.model.addObserver(this);
+  },
+
+  // this is called when component is removed destroyed
+  // good place to remove observer
+  beforeDestroy() {
+    this.model.removeObserver(this);
+  },
+
+  mounted() {
+    this.id = this.model.getSelectedDishID();
+    // when data is retrieved we update it's properties
+    // this will cause the component to re-render
+    this.model.getDetailedInfo(this.id).then(dish => {
+      this.status = "LOADED"
+      this.dish = dish
+    }).catch(() => {
+      this.status = "ERROR"
+    })
+  },
+
+  data() {
+    return {
+      status: "LOADING",
+      id: this.model.getSelectedDishID(),
+      dish: {},
+      text: this.model.getNumberOfGuests(),
+    }
+  },
+
+  methods: {
+    update() {
     },
-
-    // this is called when component is removed destroyed
-    // good place to remove observer
-    beforeDestroy() {
-      this.model.removeObserver(this);
-    },
-
-    mounted() {
-      this.id = this.model.getSelectedDishID();
-      console.log(this.id);
-      // when data is retrieved we update it's properties
-      // this will cause the component to re-render
-      this.model.getDetailedInfo(this.id).then(dish => {
-        this.status = "LOADED"
-        this.dish = dish
-      }).catch(() => {
-        this.status = "ERROR"
-      })
-    },
-
-    data() {
-      return {
-        status: "LOADING",
-        id: this.model.getSelectedDishID(),
-        dish: [],
-        text: this.model.getNumberOfGuests(),
-      }
-    },
-
-    methods: {
-      update() {
-
-      },
-      add() {
-        this.model.addDishToMenu(this.dish);
-      }
+    add() {
+      this.model.addDishToMenu(this.dish);
     }
   }
+}
 </script>
