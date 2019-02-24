3f6e49480c9511cde09abdee5a1b4e45917c213d
joarse <joarse@kth.se>
2019-02-24 12:51:41 +0100

    routing to overview and print

--- src/router.js
+++ src/router.js
@@ -3,6 +3,9 @@ import Router from "vue-router";
 import Welcome from "@/components/Welcome";
 import SelectDish from "@/components/SelectDish";
 import DishDetail from "@/components/DishDetail";
+import Overview from "@/components/Overview";
+import Print from "@/components/Print";
+
 import modelInstance from "@/data/DinnerModel";
 
 Vue.use(Router);
@@ -25,6 +28,18 @@ export default new Router({
       name: "DishDetail",
       component: DishDetail,
       props: { model: modelInstance }
+    },
+    {
+      path: "/overview",
+      name: "Overview",
+      component: Overview,
+      props: {model: modelInstance}
+    },
+    {
+      path: "/print",
+      name: "Print",
+      component: Print,
+      props: {model: modelInstance}
     },
   ]
 });
